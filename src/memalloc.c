23aeb29c959f8ff95531f9104668a879ce8cfecc
Miro Kurka <59745102+miroslavkurka@users.noreply.github.com>
2023-09-10 19:39:10 +0200

    Update memalloc.c

--- src/memalloc.c
+++ src/memalloc.c
@@ -1,10 +1,24 @@
-#include <uinstd.h>
-#include <string.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
+typedef char ALIGN[16];
 
-
-struct header_t {
-	size_t size;
-	unsigned is_free;
+union header {
+  struct header_t {
+    size_t size;
+    unsigned is_free;
+    struct header_t *next;
+  } s;
+  ALIGN stub;
 };
+
+void *malloc(size_t size) {
+  void *block;
+  block = sbrk(size);
+  if (block == (void *)-1) {
+    return NULL;
+  }
+  return block;
+}
