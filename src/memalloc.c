c4b8ed8a69bbf07e8101e673e6abe0925be4c9b3
Miro Kurka <59745102+miroslavkurka@users.noreply.github.com>
2023-09-23 19:21:07 +0200

    Update memalloc.c

--- src/memalloc.c
+++ src/memalloc.c
@@ -1,6 +1,7 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/_pthread/_pthread_mutex_t.h>
 #include <unistd.h>
 
 typedef char ALIGN[16];
@@ -14,11 +15,55 @@ union header {
   ALIGN stub;
 };
 
+typedef union header header_t;
+pthread_mutex_t global_malloc_lock;
+
 void *malloc(size_t size) {
+  size_t total_size;
   void *block;
-  block = sbrk(size);
-  if (block == (void *)-1) {
+  header_t *header;
+  if (!size)
+    return NULL;
+  pthread_mutex_lock(&global_malloc_lock);
+  header = get_free_block(size);
+
+  if (header) {
+    header-> s.is_free=0;
+    pthread_mutex_lock(&global_malloc_lock);
+    return (void*)(header +1);
+
+  }
+  total_size = sizeof(header_t) + size;
+  block = sbrk(total_size);
+
+  if (block == (void*) -1) {
+    pthread_mutex_lock(&global_malloc_lock);
     return NULL;
   }
-  return block;
+
+  header= block;
+  header->s.size = size;
+  header-> s.is_free = 0;
+  header-> s.next = NULL;
+
+  if (!head)
+    head = header;
+  if (tail)
+    tail->s.next = header; 
+
+  tail = header; 
+  pthread_mutex_lock(&global_malloc_lock);
+  return (void*) (header +1); 
+
+}
+
+header_t *get_free_block(size_t size)
+{
+  header_t *curr = head; 
+  while (curr){
+    if (curr->s.is_free && curr->s.size >= size)
+      return curr;
+    curr = curr->s.next;
+  }
+  return NULL;
 }
